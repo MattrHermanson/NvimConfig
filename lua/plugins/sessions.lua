762b553c7c5342a7d63e195f04ccb0d88b4ed154
MattrHermanson <matth2004@outlook.com>
2025-10-24 20:16:30 -0700

    added sessions.lua and messed with workspaces config

---
+++ lua/plugins/sessions.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "natecraddock/sessions.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      require("sessions").setup({
+        -- autocmd events which trigger a session save
+        --
+        -- the default is to only save session files before exiting nvim.
+        -- you may wish to also save more frequently by adding "BufEnter" or any
+        -- other autocmd event
+        events = { "VimLeavePre", "BufEnter" },
+
+        -- default session filepath
+        --
+        -- if a path is provided here, then the path argument for commands and API
+        -- functions will use session_filepath as a default if no path is provided.
+        session_filepath = "",
+
+        -- treat the default session filepath as an absolute path
+        -- if true, all session files will be stored in a single directory
+        absolute = false,
+      })
+    end,
+  },
+}
--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -46,11 +46,18 @@ return {
           add = {},
           remove = {},
           rename = {},
-          open_pre = {},
-          open = {},
+          open_pre = {
+            -- If recording, save current session state and stop recording
+            "SessionsStop",
+
+            -- delete all buffers (does not save changes)
+            "silent %bdelete!",
+          },
+          open = function()
+            require("sessions").load(nil, { silent = true })
+          end,
         },
       })
-      vim.cmd("colorscheme vague")
     end,
   },
 }
